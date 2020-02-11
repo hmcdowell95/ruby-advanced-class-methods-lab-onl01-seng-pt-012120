69ef4b8222660c03fbd16a3fb94b5687ebd27024
Harley McDowell <hmcdowell95@gmail.com>
2020-02-11 04:25:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,8 +34,9 @@ class Song
   end
   
   def self.find_or_create_by_name(name)
-    if self.find_by_name(name) 
-      return self.name
+    song = self.find_by_name(name)
+    if song
+      song
     else
       self.create_by_name(name)
     end
